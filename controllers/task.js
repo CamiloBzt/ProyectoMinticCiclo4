6dccde65f36156250d8d9c0d66f8a019625193ae
camilobzt <camilobzt@gmail.com>
2021-07-23 21:31:53 -0500

    edit_and_delete_methods

--- controllers/task.js
+++ controllers/task.js
@@ -55,10 +55,58 @@ var controller = {
           if (!tasks){
             return res.status(404).send({ status: "error", message: "No Data" });
           }
-        return res.status(200).send({ status: "error", tasks });
+        return res.status(200).send({ status: "success", tasks });
       })
     
+  },
+  edit: (req, res) => {
+    // Obtener id de la tarea
+    var taskId = req.params.id;
+
+    // Obtener los datos que llegan por PUT
+    var params = req.body;
+
+    // Validar datos
+    try{
+      var validate_title = !validator.isEmpty(params.title);
+      var validate_description = !validator.isEmpty(params.description);
+    }catch(err){
+      return res.status(200).send({ status: "error", message: "Incorrect Data" });  
+    }
+
+    if (validate_title && validate_description){
+      // Find and Update
+      Task.findOneAndUpdate({_id: taskId}, params, {new:true}, (err, taskUpdated) => {
+        if(err){
+          return res.status(500).send({ status: "error", message: "Update Error" });  
+        }
+        if (!taskUpdated){
+          return res.status(404).send({ status: "error", message: "No Data" });  
+        }
+        return res.status(200).send({ status: "success", task: taskUpdated });  
+      })
+    }else{
+      // Devolver respuesta
+      return res.status(200).send({ status: "error", message: "Incorrect Data" });  
+    }
+  },
+
+  delete: (req, res) => {
+    // Obtener id
+    var taskId = req.params.id;
+
+    // Find and delete
+    Task.findOneAndDelete({_id: taskId}, (err, taskRemoved) => {
+      if (err){
+        return res.status(500).send({ status: "error", message: "Remove Error" });  
+      }
+      if (!taskRemoved){
+        return res.status(404).send({ status: "error", message: "No Data" });
+      }
+      return res.status(200).send({ status: "success", task: taskRemoved});
+    });
   }
-};
+
+}; // end controller
 
 module.exports = controller;
--- routes/task.js
+++ routes/task.js
@@ -12,5 +12,7 @@ router.post('/test-de-controlador', TaskController.test);
 // Rutas
 router.post('/save', TaskController.save);
 router.get('/tasks', TaskController.getTasks);
+router.put('/task/:id', TaskController.edit);
+router.delete('/task/:id', TaskController.delete);
 
 module.exports = router;
