fdfc87baf71973560b1c3928c8a6079a9a70a417
camilobzt <camilobzt@gmail.com>
2021-07-29 17:56:48 -0500

    method_users

--- controllers/task.js
+++ controllers/task.js
@@ -10,7 +10,7 @@ var controller = {
   save: async (req, res) => {
     // Recibir id del usuario
     var userId = req.params.id;
-    var user = await Task.findOne({_id: userId});
+    var user = await Task.findOne({ _id: userId });
     // Parametros Post
     var params = req.body;
 
@@ -30,9 +30,10 @@ var controller = {
         title: String,
         description: String,
         date: {
-            type: Date, default: Date.now
+          type: Date,
+          default: Date.now,
         },
-        priority: Number
+        priority: Number,
       };
 
       // Asignar valores
@@ -43,80 +44,101 @@ var controller = {
 
       user.tareas.push(task);
       // Guardar la tarea
-      Task.findOneAndUpdate({_id: userId}, user, {new:true}, (err, taskUpdated) => {
-        if(err){
-          return res.status(500).send({ status: "error", message: "Update Error" });  
+      Task.findOneAndUpdate(
+        { _id: userId },
+        user,
+        { new: true },
+        (err, taskUpdated) => {
+          if (err) {
+            return res
+              .status(500)
+              .send({ status: "error", message: "Update Error" });
+          }
+          if (!taskUpdated) {
+            return res
+              .status(404)
+              .send({ status: "error", message: "No Data" });
+          }
+          return res.status(200).send({ status: "success", task: taskUpdated });
         }
-        if (!taskUpdated){
-          return res.status(404).send({ status: "error", message: "No Data" });  
-        }
-        return res.status(200).send({ status: "success", task: taskUpdated });  
-      })
+      );
     } else {
       return res.status(200).send({ status: "error", message: "Invalid Data" });
     }
   },
 
-store : async (req,res,next) => {
+  store: async (req, res, next) => {
     try {
       var params = req.body;
-      const user = await Task.findOne({correo: req.body.correo})
+      const user = await Task.findOne({ correo: req.body.correo });
       // console.log(user);
       if (user) {
         res.status(409).send({
-          message: "Sorry the email is already use" + req.body.correo+
-          " and there is a "  
-                })}
-      else{
-          if (req.body.contraseña1 !== req.body.contraseña2){
-            res.status(409).send({
-              message: "Las contraseñas no coinciden"  
-                    })
-          }
-          else{
-                // const user = await task.save(req.body);
-                let task = await new Task({
-                  name: req.body.name,
-                  correo: req.body.correo,
-                  contraseña1 : req.body.contraseña1,
-                  // contraseña2 : req.body.contraseña2,
-                  pais : req.body.pais
-              })
-              
-              task.save()
-          
-              .then(response => {
-                  res.json({
-                      message: 'Added Succesfully'
-                  })
-          
-              })
+          message:
+            "Sorry the email is already use" +
+            req.body.correo +
+            " and there is a ",
+        });
+      } else {
+        if (req.body.contraseña1 !== req.body.contraseña2) {
+          res.status(409).send({
+            message: "Las contraseñas no coinciden",
+          });
+        } else {
+          // const user = await task.save(req.body);
+          let task = await new Task({
+            name: req.body.name,
+            correo: req.body.correo,
+            contraseña1: req.body.contraseña1,
+            // contraseña2 : req.body.contraseña2,
+            pais: req.body.pais,
+          });
 
-            }
-          }
-          // task.save()
+          task
+            .save()
+
+            .then((response) => {
+              res.json({
+                message: "Added Succesfully",
+              });
+            });
+        }
+      }
+      // task.save()
       // }
     } catch (error) {
       res.status(500).send({
-        message: "Error --> " 
-    })
-    next(error);
+        message: "Error --> ",
+      });
+      next(error);
     }
-},
-
+  },
+  getUsers: (req, res) => {
+    // Find
+    Task.find({})
+      .sort('_id')
+      .exec((err, users) => {
+        if (err) {
+          return res.status(500).send({ status: "error", message: "Error" });
+        }
+        if (!users) {
+          return res.status(404).send({ status: "error", message: "No Data" });
+        }
+        return res.status(200).send({ status: "success", users });
+      });
+  },
   getTasks: (req, res) => {
-      // Find
-      Task.findOne({_id: req.params.id}).exec((err, task) => {
-          if (err){
-            return res.status(500).send({ status: "error", message: "Error" });
-          }
-          if (!task){
-            return res.status(404).send({ status: "error", message: "No Data" });
-          }
-          var tareas = task.tareas;
-        return res.status(200).send({ status: "success", tareas });
-      })
-    
+    // Find
+    Task.findOne({ _id: req.params.id }).exec((err, task) => {
+      if (err) {
+        return res.status(500).send({ status: "error", message: "Error" });
+      }
+      if (!task) {
+        return res.status(404).send({ status: "error", message: "No Data" });
+      }
+      var tareas = task.tareas;
+      return res.status(200).send({ status: "success", tareas });
+    });
   },
   edit: async (req, res) => {
     // Obtener id de la tarea
@@ -126,16 +148,18 @@ store : async (req,res,next) => {
     var params = req.body;
 
     // Validar datos
-    try{
+    try {
       var validate_title = !validator.isEmpty(params.title);
       var validate_description = !validator.isEmpty(params.description);
-    }catch(err){
-      return res.status(200).send({ status: "error", message: "Incorrect Data" });  
+    } catch (err) {
+      return res
+        .status(200)
+        .send({ status: "error", message: "Incorrect Data" });
     }
 
-    if (validate_title && validate_description){
+    if (validate_title && validate_description) {
       // Find and Update
-      var user = await Task.findOne({_id: userId});
+      var user = await Task.findOne({ _id: userId });
       var index = params.indice;
       var tarea = user.tareas[index];
       tarea.title = params.title;
@@ -143,18 +167,29 @@ store : async (req,res,next) => {
       tarea.priority = params.priority;
       tarea.date = params.date;
       user.tareas[index] = tarea;
-      Task.findOneAndUpdate({_id: userId}, user, {new:true}, (err, taskUpdated) => {
-        if(err){
-          return res.status(500).send({ status: "error", message: "Update Error" });  
+      Task.findOneAndUpdate(
+        { _id: userId },
+        user,
+        { new: true },
+        (err, taskUpdated) => {
+          if (err) {
+            return res
+              .status(500)
+              .send({ status: "error", message: "Update Error" });
+          }
+          if (!taskUpdated) {
+            return res
+              .status(404)
+              .send({ status: "error", message: "No Data" });
+          }
+          return res.status(200).send({ status: "success", task: taskUpdated });
         }
-        if (!taskUpdated){
-          return res.status(404).send({ status: "error", message: "No Data" });  
-        }
-        return res.status(200).send({ status: "success", task: taskUpdated });  
-      })
-    }else{
+      );
+    } else {
       // Devolver respuesta
-      return res.status(200).send({ status: "error", message: "Incorrect Data" });  
+      return res
+        .status(200)
+        .send({ status: "error", message: "Incorrect Data" });
     }
   },
 
@@ -162,22 +197,27 @@ store : async (req,res,next) => {
     // Obtener id
     var userId = req.params.id;
     var params = req.body;
-    var user = await Task.findOne({_id: userId});
+    var user = await Task.findOne({ _id: userId });
     var index = params.indice;
     user.tareas.splice(index, 1);
     // Find and delete
-    Task.findOneAndUpdate({_id: userId}, user, {new:true}, (err, taskUpdated) => {
-      if(err){
-        return res.status(500).send({ status: "error", message: "Update Error" });  
+    Task.findOneAndUpdate(
+      { _id: userId },
+      user,
+      { new: true },
+      (err, taskUpdated) => {
+        if (err) {
+          return res
+            .status(500)
+            .send({ status: "error", message: "Update Error" });
+        }
+        if (!taskUpdated) {
+          return res.status(404).send({ status: "error", message: "No Data" });
+        }
+        return res.status(200).send({ status: "success", task: taskUpdated });
       }
-      if (!taskUpdated){
-        return res.status(404).send({ status: "error", message: "No Data" });  
-      }
-      return res.status(200).send({ status: "success", task: taskUpdated });  
-    })
-  }
-
-
+    );
+  },
 }; // end controller
 
 module.exports = controller;
--- routes/task.js
+++ routes/task.js
@@ -8,7 +8,7 @@ var router = express.Router();
 // Rutas prueba
 router.get('/test-de-controlador', TaskController.test);
 router.post('/test-de-controlador', TaskController.test);
-
+router.get('/users', TaskController.getUsers);
 // Rutas
 router.put('/saveTask/:id', TaskController.save);
 router.get('/tasks/:id', TaskController.getTasks);
